14d108fe569d8dceab8b8b5dbc93548fd0df47a6
Arthur van der Peijl <aavanderpeijl@gmail.com>
2016-01-05 21:58:53 +0100

    Compile netatalk instead of pkgin

--- copy/var/zoneinit/includes/60-netatalk.sh
+++ copy/var/zoneinit/includes/60-netatalk.sh
@@ -1,7 +1,23 @@
-log "creating database"
+#!/bin/bash
+# manual script converted into bash shell:
 
-mkdir -p /var/db/netatalk/CNID
+log "download source-code"
 
-log "starting netatalk"
+pushd /tmp
+wget http://sourceforge.net/projects/netatalk/files/netatalk/3.1.7/netatalk-3.1.7.tar.gz
 
-/usr/sbin/svcadm enable netatalk
+log "unpack source-code"
+
+tar xfvz netatalk-3.1.7.tar.gz
+
+log "compile source-code"
+
+cd netatalk-3.1.7
+./configure --with-ssl-dir=/opt/local --with-libevent-header=/opt/local --with-libevent-lib=/opt/local --with-libgcrypt --with-bdb=/opt/local --with-init-style=solaris --with-init-dir=/var/svc/manifest/network/ --without-pam --prefix=/opt/local
+ln -s /opt/local/lib /opt/local/lib/64
+make && make install
+
+popd
+
+svcadm enable svc:/network/dns/multicast:default
+svcadm enable svc:/network/netatalk:default
