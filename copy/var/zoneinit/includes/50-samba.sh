4a9f2f6c91ba04db3d2e09588172859fa3ae6017
Arthur van der Peijl <aavanderpeijl@gmail.com>
2017-08-19 13:11:24 +0200

    Delete 50-samba.sh

--- copy/var/zoneinit/includes/50-samba.sh
+++
@@ -1,9 +0,0 @@
-log "force correct ownership of /data directory"
-
-# chown -R dsapid:dsapid /data
-
-log "starting samba"
-
-/usr/sbin/svcadm enable samba:nmbd
-/usr/sbin/svcadm enable samba:smbd
-/usr/sbin/svcadm enable dns/multicast
